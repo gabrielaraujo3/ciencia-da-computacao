a48825dd87019a9b359aa3c0554817208f498151
gabrielaraujo3 <araujog388@gmail.com>
2022-05-02 23:43:57 -0300

    Aula Prolog parte 2

---
+++ Prolog/family.pl
@@ -0,0 +1,15 @@
+%FATOS
+gerou(carlos,fabiana).
+gerou(carlos,felipe).
+gerou(maria,carlos).
+gerou(antonio,carlos).
+gerou(maria,cecilia).
+gerou(antonio,cecilia).
+gerou(cecilia,aline).
+gerou(marcos,aline).
+gerou(aline,camila).
+gerou(marcos,sabrina).
+
+%REGRAS
+ancestral(X,Y):-gerou(X,Y).
+amcestral(X,Y):-ancestral(X,Z),gerou(Z,Y).
